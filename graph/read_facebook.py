f450f671a01292b0bf0cc625c58992f172921b8c
Jungwoo (Jeff) Park <jwp@berkeley.edu>
2018-12-05 13:11:51 -0800

    Update read_facebook.py

--- graph/read_facebook.py
+++ graph/read_facebook.py
@@ -4,4 +4,4 @@ import matplotlib.pyplot as plt
 g = nx.read_edgelist('facebook_combined.txt', create_using=nx.Graph(), nodetype=int)
 print(nx.info(g))
 sp=nx.spring_layout(g)
-ply.axis('off')
+plt.axis('off')
